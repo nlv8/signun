10e4118f2c78c249600d3a586d7d8faf9e75f164
Attila Bagossy <bagossyattila@outlook.com>
2019-05-07 10:45:37 +0200

    Created a dummy native module exposing an empty object

---
+++ binding.gyp
@@ -0,0 +1,8 @@
+{
+    "targets": [{
+        "target_name": "signun",
+        "sources": [
+            "./module/native/src/signun_node.c"
+        ]
+    }]
+}
---
+++ module/native/src/signun_node.c
@@ -0,0 +1,14 @@
+#include <node_api.h>
+#include <js_native_api.h>
+
+
+static napi_value init_signun_module(napi_env env, napi_value exports) {
+    napi_value result;
+    napi_status status;
+
+    status = napi_create_object(env, &result);
+
+    return result;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, init_signun_module)
